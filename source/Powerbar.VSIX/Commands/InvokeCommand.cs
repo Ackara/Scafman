76753a3cbafbdb4746ec945bd013a8694dd10845
Ackara <ackara.dev@outlook.com>
2019-03-27 09:23:40 -0400

    Add symbols parser

---
+++ source/Powerbar.VSIX/Commands/InvokeCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+
+namespace Acklann.Powerbar.Commands
+{
+    public class InvokeCommand
+    {
+        public InvokeCommand()
+        {
+
+        }
+
+        public void Execute(string command, Context context)
+        {
+
+        }
+
+        private void OnCommandInvoked(object sender, EventArgs e)
+        {
+
+        }
+
+        //internal static void Initialize(IMenuCommandService commandService, DTE dte)
+        //{
+
+        //}
+
+
+        #region Singleton
+        public static InvokeCommand Instance { get; private set; }
+
+        
+        #endregion Singleton
+
+    }
+}
