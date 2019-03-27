7675b8b0251d7293e8f756e131d2c34369cbfb8a
Ackara <ackara.dev@outlook.com>
2019-03-27 09:53:32 -0400

    Get vsix project to compile

--- source/Powerbar.VSIX/Commands/InvokeCommand.cs
+++ source/Powerbar.VSIX/Commands/InvokeCommand.cs
@@ -1,42 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
 using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.Shell;
+using System.Globalization;
+using Task = System.Threading.Tasks.Task;
 
-
-namespace Acklann.Powerbar.Commands
+namespace Acklann.Powerbar
 {
-    public class InvokeCommand
+    internal sealed class InvokeCommand
     {
-        public InvokeCommand()
+        private InvokeCommand(AsyncPackage package, IMenuCommandService commandService)
         {
-
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            commandService.AddCommand(new MenuCommand(OnCommandInvoked, new CommandID(Symbol.CmdSet.Guid, Symbol.CmdSet.InvokeCommandId)));
         }
 
-        public void Execute(string command, Context context)
-        {
-
-        }
-
-        private void OnCommandInvoked(object sender, EventArgs e)
-        {
-
-        }
-
-        //internal static void Initialize(IMenuCommandService commandService, DTE dte)
-        //{
-
-        //}
-
-
-        #region Singleton
         public static InvokeCommand Instance { get; private set; }
 
-        
-        #endregion Singleton
+        internal static async Task InitializeAsync(AsyncPackage package, IMenuCommandService commandService)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            Instance = new InvokeCommand(package, commandService);
+        }
 
+        internal void OnCommandInvoked(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "Command1";
+
+            VsShellUtilities.ShowMessageBox(
+                this._package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        #region Private Members
+
+        private readonly AsyncPackage _package;
+
+        #endregion Private Members
     }
 }
---
+++ source/Powerbar.VSIX/Symbol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Acklann.Powerbar
+{
+	public static class Symbol
+	{
+		public struct Package
+		{
+			public const string GuidString = "3c078121-2119-42af-9055-50abb4af5afe";
+			public static readonly Guid Guid = new Guid("3c078121-2119-42af-9055-50abb4af5afe");
+		}
+		public struct CmdSet
+		{
+			public const string GuidString = "069838a4-a734-4fed-bc02-ef643489a6e7";
+			public static readonly Guid Guid = new Guid("069838a4-a734-4fed-bc02-ef643489a6e7");
+			public const int VSToolsMenuGroup = 0x1020;
+			public const int FileContextMenuGroup = 0x101;
+			public const int ProjectContextMenuGroup = 0x102;
+			public const int ToolsMenuId = 0x201;
+			public const int InvokeCommandId = 0x300;
+		}
+	}
+}
