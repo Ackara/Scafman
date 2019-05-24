0abe2a4cb1db82d65b5e9791713e051ea9e7100b
Ackara <ackara.dev@outlook.com>
2019-05-23 22:27:30 -0400

    Fix syntax error

--- source/Templata.VSIX/Properties/AssemblyInfo.cs
+++ source/Templata.VSIX/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("0.0.2")]
 [assembly: AssemblyVersion("0.0.2")]
 [assembly: AssemblyFileVersion("0.0.2")]
-[assembly: AssemblyInformationalVersion("0.0.2")
+[assembly: AssemblyInformationalVersion("0.0.2")]
