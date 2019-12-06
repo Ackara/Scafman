9566db8213b0e931e7f1f5158377a55a7abfd082
Ackara <ackara.spam@outlook.com>
2019-12-06 10:14:36 -0400

    Change the version number to '0.0.28'

--- src/Scafman.VSIX/Properties/AssemblyInfo.cs
+++ src/Scafman.VSIX/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Scafman.VSIX")]
+[assembly: AssemblyProduct("Scafman")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.28")]
+[assembly: AssemblyFileVersion("0.0.28")]
